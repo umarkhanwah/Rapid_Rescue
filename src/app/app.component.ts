50f130607bc333f6fb6244f486c772070f125e3e
umarkhanwah <umarkhanwah044@gmail.com>
2024-09-21 15:15:08 +0500

    Firt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/js/slider.js
@@ -0,0 +1,16 @@
+$('#datepicker').datepicker({
+    iconsLibrary: 'fontawesome',
+    icons: {
+        rightIcon: '<span class="fa fa-caret-down"></span>'
+    }
+});
+$('#datepicker2').datepicker({
+    iconsLibrary: 'fontawesome',
+    icons: {
+        rightIcon: '<span class="fa fa-caret-down"></span>'
+    }
+
+});
+$(document).ready(function() {
+$('.js-example-basic-multiple').select2();
+});
